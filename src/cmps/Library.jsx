37fa7221247ce196bc6430699c1ae998d0423907
Adar <adarsarusi5@hotmail.com>
2026-06-15 22:07:24 +0300

    Setting up layout for work

---
+++ src/cmps/Library.jsx
@@ -0,0 +1,11 @@
+export function Library() {
+    return (
+        <section className="app-library">
+            <h1>Library sweet Library</h1>
+            
+
+            
+        </section >
+    )
+}
+
---
+++ src/pages/Explore.jsx
@@ -0,0 +1,11 @@
+export function Explore() {
+    return (
+        <section className="explore dynamic-area">
+            <h1>Explore sweet Explore</h1>
+            
+
+            
+        </section >
+    )
+}
+
