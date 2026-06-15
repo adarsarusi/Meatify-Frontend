28984b497b987fdc42dec8414e0c3788440b381f
Itay <itaymoyal31@gmail.com>
2026-06-15 16:11:47 +0300

    Implement AppHeader component and enhance styling; update CSS imports

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <div className="main-container">
-        {/* <AppHeader /> */}
+        <AppHeader />
         {/* <UserMsg /> */}
 
         <main>
