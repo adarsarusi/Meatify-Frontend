d12ad42d9baadb8c040ecf1592b60e64e96451ad
Denis Mokin <denmokin@gmail.com>
2026-06-23 15:02:22 +0300

    Fixed AppHeader dropdown

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -97,7 +97,7 @@ export function AppHeader() {
                 <IconComp name="browse" className="icon--muted" />
               </button>
             </div>
-            {/* {isOpen && <SerachResultsDropdown stations={stations} />} */}
+            <SerachResultsDropdown stations={stations} />
           </div>
         </div>
 
