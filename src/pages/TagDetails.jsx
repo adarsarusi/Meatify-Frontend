4d41a8c325a678b43699b03d0c3db158f56b2fb9
Adar <adarsarusi5@hotmail.com>
2026-06-27 22:50:41 +0300

    Browse now shows stations as well -fixed queue's height to not block scrolling -added browse details page color

--- src/pages/TagDetails.jsx
+++ src/pages/TagDetails.jsx
@@ -1,20 +1,45 @@
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import SongList from '../cmps/globalCmps/SongList'
+import { SongList } from '../cmps/globalCmps/SongList'
+import { SquarePreview } from '../cmps/globalCmps/SquarePreview'
+import { TAGS_DATA } from '../services/station/station.service.local'
 
 export function TagDetails() {
     const { tag } = useParams()
+
+    const stations = useSelector(
+        storeState => storeState.stationModule.stations
+    )
+
+    const filteredStations = stations.filter(station =>
+        station.tags?.some(
+            stationTag =>
+                stationTag.toLowerCase() === tag.toLowerCase()
+        )
+    )
 
     const songs = useSelector(
         storeState => storeState.songModule.songs
     )
 
     const filteredSongs = songs.filter(song =>
-        song.tags?.includes(tag)
+        song.tags?.some(
+            songTag =>
+                songTag.toLowerCase() === tag.toLowerCase()
+        )
+    )
+
+    const tagData = TAGS_DATA.find(
+        currTag => currTag.title === tag
     )
 
     return (
-        <section className="tag-details dynamic-area">
+        <section
+            className="tag-details dynamic-area"
+            style={{
+                '--tag-color': tagData?.color || '#509BF5',
+            }}>
+
             <header className="tag-details__header">
                 <img
                     src={`/tags/${tag
@@ -26,11 +51,29 @@ export function TagDetails() {
                 <div>
                     <p>Genre</p>
                     <h1>{tag}</h1>
-                    <p>{filteredSongs.length} songs</p>
+                    <p>{filteredStations.length} stations · {filteredSongs.length} songs</p>
                 </div>
             </header>
 
-            <SongList songs={filteredSongs} />
+            <div className="tag-details__lists">
+                {filteredStations.length > 0 && (
+                    <div className="tag-details__station-list">
+                        <h2>Stations</h2>
+                        <ul className='demo-square-list'>
+                            {filteredStations.map(station =>
+
+                                <li key={station._id}>
+                                    <SquarePreview entity={station} />
+                                </li>)}
+                        </ul>
+                    </div>
+                )}
+                <div className="tag-details__song-list">
+                    <h2>Songs</h2>
+                    <SongList songs={filteredSongs} />
+                </div>
+            </div>
+
         </section>
     )
 }
