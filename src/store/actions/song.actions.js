ee59186935a4ae2d9ee980a2776a1311f38080ad
Denis Mokin <denmokin@gmail.com>
2026-07-11 19:34:35 +0300

    Removed unused actions

--- src/store/actions/song.actions.js
+++ src/store/actions/song.actions.js
@@ -16,36 +16,6 @@ export async function loadSongs(filterBy = {}) {
     }
 }
 
-
-export async function saveSongStationId(song) {
-    try {
-        const savedSong = await songService.updateSongStation(song)
-        store.dispatch(getCmdUpdateSong(savedSong))
-        return savedSong
-    } catch (err) {
-        console.log("Cannot update song", err)
-        throw err
-    }
-}
-export async function removeSongStationId(song) {
-    try {
-        const savedSong = await songService.updateSongStation(song)
-        store.dispatch(getCmdUpdateSong(savedSong))
-        return savedSong
-    } catch (err) {
-        console.log("Cannot update song", err)
-        throw err
-    }
-}
-
-
-function getCmdUpdateSong(song) {
-    return {
-        type: UPDATE_SONGS,
-        song,
-    }
-}
-
 function getCmdGetSongs(songs) {
     return {
         type: SET_SONGS,
