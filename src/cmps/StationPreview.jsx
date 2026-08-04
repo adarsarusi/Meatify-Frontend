5aa22552bdc552a5765c600ee5e80ea97020ffc4
Denis Mokin <denmokin@gmail.com>
2026-08-04 13:55:59 +0300

    Fix shuffle functionality in StationPreview and StationOptions components

--- src/cmps/StationPreview.jsx
+++ src/cmps/StationPreview.jsx
@@ -4,7 +4,11 @@ import { useSelector } from 'react-redux'
 import { IconComp } from './globalCmps/IconComp'
 import { LikeBtn } from './LikeBtn'
 import { StationCover } from './globalCmps/StationCover'
+
 import { setQueue, setCurrentSong, setPlayingStation, toggleIsPlaying } from "../store/actions/player.actions.js"
+import { TOGGLE_IS_SHUFFLE} from "../store/reducers/player.reducer.js"
+import { store } from '../store/store.js'
+
 
 export function StationPreview({ station, isSearch }) {
     const location = useLocation()
@@ -16,6 +20,8 @@ export function StationPreview({ station, isSearch }) {
     const isPlaying = useSelector((storeState) => storeState.playerModule.isPlaying)
     const songs = useSelector((storeState) => storeState.songModule.songs) || []
     const loggedinUser = useSelector(storeState => storeState.userModule.user)
+    const isShuffle = useSelector((storeState) => storeState.playerModule.isShuffle)
+
 
     const isLikedSongsStation = station?.tags?.includes("Liked")
     const isCurrStationPlaying = currPlayingStation?._id === station?._id
@@ -23,10 +29,12 @@ export function StationPreview({ station, isSearch }) {
 
     const stationSongs = useMemo(() => {
         if (!station?.songs?.length) return []
-        return songs.filter(song => station.songs.includes(song._id.toString()))
+        return station.songs
+            .map(songId => songs.find(song => song._id.toString() === songId))
+            .filter(Boolean)
     }, [songs, station?.songs])
 
-    const songCount = loggedinUser?.likedSongIds?.length || 0
+    const likedSongsCount = loggedinUser?.likedSongIds?.length || 0
 
     if (!station) return null
 
@@ -36,6 +44,7 @@ export function StationPreview({ station, isSearch }) {
             toggleIsPlaying()
         } else {
             setQueue(stationSongs)
+            store.dispatch({ type: 'TOGGLE_IS_SHUFFLE', isShuffle: false })
             setPlayingStation(station)
             if (stationSongs.length > 0) {
                 setCurrentSong(stationSongs[0])
@@ -66,7 +75,7 @@ export function StationPreview({ station, isSearch }) {
                 </p>
 
                 {isLikedSongsStation ? (
-                    <p className='station-preview__song-length ellipsis-text'>{songCount} songs</p>
+                    <p className='station-preview__song-length ellipsis-text'>{likedSongsCount} songs</p>
                 ) : (
                     <p className='station-preview__creator-name ellipsis-text'>{station?.createdBy?.fullname}</p>
                 )}
--- src/cmps/globalCmps/StationOptions.jsx
+++ src/cmps/globalCmps/StationOptions.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react"
 import { useSelector } from "react-redux"
 import { setQueue, setCurrentSong, setPlayingStation, toggleIsPlaying, toggleShuffleQueue } from "../../store/actions/player.actions"
+import { TOGGLE_IS_SHUFFLE } from "../../store/reducers/player.reducer"
 
 import { IconComp } from "./IconComp"
 import { LikeBtn } from "../LikeBtn"
@@ -34,6 +35,21 @@ export function StationOptions({ station, stationSongs, isOwner, onEditStation, 
         }
     }, [])
 
+    
+
+    const onPlayStation = (ev) => {
+        ev.stopPropagation()
+        if (isCurrStationPlaying) {
+            toggleIsPlaying()
+        } else {
+            setQueue(stationSongs)
+store.dispatch({type:'TOGGLE_IS_SHUFFLE', isShuffle: false})
+            setPlayingStation(station)
+            if (stationSongs.length > 0) {
+                setCurrentSong(stationSongs[0])
+            }
+        }
+    }
 
 
     return (
@@ -41,15 +57,7 @@ export function StationOptions({ station, stationSongs, isOwner, onEditStation, 
             <div className="station-options__btn-container">
                 {station?.songs?.length > 0 && <button
                     className="station-options__play-btn btn play-btn green-btn "
-                    onClick={() => {
-                        if (isCurrStationPlaying) {
-                            toggleIsPlaying()
-                        } else {
-                            setQueue(stationSongs)
-                            setCurrentSong(stationSongs[0])
-                            setPlayingStation(station)
-                        }
-                    }}
+                    onClick={onPlayStation}
                 >   {(isPlaying && isCurrStationPlaying) ?
                     <IconComp name="pause" className="icon--md" />
                     : <IconComp name="play" className="icon--md" />
